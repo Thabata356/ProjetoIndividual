e2374c779dc616008fa5eb4e40e980c301b9bb7c
MarianaXavier2 <mariana.xavier@sptech.school>
2026-06-12 18:10:52 -0300

    :sparkles: Feat: Arrumar o gráfico dashboard.

--- web-data-viz/src/controllers/medidaController.js
+++ web-data-viz/src/controllers/medidaController.js
@@ -4,11 +4,11 @@ function buscarUltimasMedidas(req, res) {
 
     const limite_linhas = 7;
 
-    var idLivros = req.params.idLivros;
+    var idLivro = req.params.idLivro;
 
     console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
 
-    medidaModel.buscarUltimasMedidas(idLivros, limite_linhas).then(function (resultado) {
+    medidaModel.buscarUltimasMedidas(idLivro, limite_linhas).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -24,11 +24,11 @@ function buscarUltimasMedidas(req, res) {
 
 function buscarMedidasEmTempoReal(req, res) {
 
-    var idLivros = req.params.idLivros;
+    var idLivro = req.params.idLivro;
 
     console.log(`Recuperando medidas em tempo real`);
 
-    medidaModel.buscarMedidasEmTempoReal(idLivros).then(function (resultado) {
+    medidaModel.buscarMedidasEmTempoReal(idLivro).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
--- web-data-viz/src/models/medidaModel.js
+++ web-data-viz/src/models/medidaModel.js
@@ -1,29 +1,28 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idLivros, limite_linhas) {
+function buscarUltimasMedidas(idLivro, limite_linhas) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idAquario}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
+            paginas_lidas,
+            data_registro,
+                        DATE_FORMAT(data_registro, '%H:%i:%s') AS momento_grafico
+                    FROM historico_leitura 
+                    WHERE fk_livro = ${idLivro}
+                    ORDER BY data_registro DESC LIMIT ${limite_linhas}`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarMedidasEmTempoReal(idLivros) {
+function buscarMedidasEmTempoReal(idLivro) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${idAquario} 
-                    ORDER BY id DESC LIMIT 1`;
+            paginas_lidas,
+            data_registro,
+                        DATE_FORMAT(data_registro, '%H:%i:%s') AS momento_grafico
+                    FROM historico_leitura 
+                    WHERE fk_livro = ${idLivro}
+                    ORDER BY data_registro DESC LIMIT 1`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
