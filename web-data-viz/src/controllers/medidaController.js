092874c0ea576dbdea7a490a1381254e1df12357
Thabata356 <thabatav7@gmail.com>
2026-06-17 19:01:09 -0300

    :sparkles: Feat: Tentar fazer a lóca das Kpis.

--- web-data-viz/src/controllers/medidaController.js
+++ web-data-viz/src/controllers/medidaController.js
@@ -60,8 +60,26 @@ function buscarCincoGeneros(req, res) {
     });
 }
 
+function buscarDadosKpis(req, res) {
+
+    var idUsuario = req.params.idUsuario;
+
+    medidaModel.buscarDadosKpis(idUsuario).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas dos cinco generos.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
 module.exports = {
     buscarUltimasMedidas,
     buscarMedidasEmTempoReal,
-    buscarCincoGeneros
+    buscarCincoGeneros,
+    buscarDadosKpis
 }
--- web-data-viz/src/models/medidaModel.js
+++ web-data-viz/src/models/medidaModel.js
@@ -45,8 +45,23 @@ LIMIT 5;
     return database.executar(instrucaoSql);
 }
 
+function buscarDadosKpis(idUsuario) {
+
+    var instrucaoSql = `SELECT
+    (SELECT COUNT(id) AS total_livros FROM livro WHERE fk_usuario = ${idUsuario}) as kpi1,
+    (SELECT ROUND(AVG(paginas_lidas), 0) AS media_paginas_lidas FROM historico_leitura WHERE fk_usuario =  ${idUsuario}) as kpi2,
+    (SELECT SUM(sub.max_paginas) AS total_paginas_reais FROM (SELECT MAX(h.paginas_lidas) AS max_paginas FROM historico_leitura h JOIN livro l ON h.fk_livro = l.id WHERE h.fk_usuario = ${idUsuario} AND l.genero = 'Ficção Distópica' GROUP BY h.fk_livro) AS sub) as kpi3;
+                        `;
+
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
 module.exports = {
     buscarUltimasMedidas,
     buscarMedidasEmTempoReal,
-    buscarCincoGeneros
+    buscarCincoGeneros,
+    buscarDadosKpis
 }
